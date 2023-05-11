40dae19cb18f0f7db7ffb181df1f88ca7965adc8
Shanas <87828025+mrshanas@users.noreply.github.com>
2023-05-12 02:46:45 +0300

    :sparkles: Update urls.py

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
 from drf_yasg import openapi
@@ -32,4 +31,4 @@ urlpatterns = [
          name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # noqa
     path('schema', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

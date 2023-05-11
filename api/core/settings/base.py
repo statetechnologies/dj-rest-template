8111a84302d8fa4b62b864fe5000f15251210815
Shanas <87828025+mrshanas@users.noreply.github.com>
2023-05-12 02:47:34 +0300

    :sparkles: Update base.py

--- api/core/settings/base.py
+++ api/core/settings/base.py
@@ -108,6 +108,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
